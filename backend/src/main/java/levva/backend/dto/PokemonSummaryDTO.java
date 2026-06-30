930181716ca30bbb749f373dbd1acf7126992010
Jailton Santos <jailtonmatos200@gmail.com>
2026-06-30 16:47:47 -0300

    refactor: record change

--- backend/src/main/java/levva/backend/dto/PokemonSummaryDTO.java
+++ backend/src/main/java/levva/backend/dto/PokemonSummaryDTO.java
@@ -1,12 +1,8 @@
 package levva.backend.dto;
 
-import lombok.Builder;
-import lombok.Data;
 
-@Data
-@Builder
-public class PokemonSummaryDTO {
-    private Integer id;
-    private String name;
-    private String spriteUrl;
-}
+public record PokemonSummaryDTO(
+        Integer id,
+        String name,
+        String spriteUrl
+) {}
