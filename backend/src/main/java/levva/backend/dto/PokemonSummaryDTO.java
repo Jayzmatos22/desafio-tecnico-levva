4f653fd28b30fd165f11a981d100514180263a8f
Jailton Santos <jailtonmatos200@gmail.com>
2026-06-30 16:40:41 -0300

    feat: summary dto

--- backend/src/main/java/levva/backend/dto/PokemonSummaryDTO.java
+++ backend/src/main/java/levva/backend/dto/PokemonSummaryDTO.java
@@ -1,4 +1,12 @@
 package levva.backend.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class PokemonSummaryDTO {
+    private Integer id;
+    private String name;
+    private String spriteUrl;
 }
