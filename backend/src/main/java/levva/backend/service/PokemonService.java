b6dec89a09b35c3a640864f5273ab31ce8795117
Jailton Santos <jailtonmatos200@gmail.com>
2026-06-30 17:14:26 -0300

    feat: implementation apiClient functions

--- backend/src/main/java/levva/backend/service/PokemonService.java
+++ backend/src/main/java/levva/backend/service/PokemonService.java
@@ -1,4 +1,27 @@
 package levva.backend.service;
 
+import levva.backend.client.PokeApiClient;
+import levva.backend.dto.PokemonDetailDTO;
+import levva.backend.dto.PokemonSummaryDTO;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Service
 public class PokemonService {
+
+    private final PokeApiClient pokeApiClient;
+
+    public PokemonService(PokeApiClient pokeApiClient) {
+        this.pokeApiClient = pokeApiClient;
+    }
+
+    public Mono<List<PokemonSummaryDTO>> listPokemons(int limit, int offset) {
+        return pokeApiClient.listPokemons(limit, offset);
+    }
+
+    public Mono<PokemonDetailDTO> getPokemonDetail(String name) {
+        return pokeApiClient.getPokemonDetail(name);
+    }
 }
