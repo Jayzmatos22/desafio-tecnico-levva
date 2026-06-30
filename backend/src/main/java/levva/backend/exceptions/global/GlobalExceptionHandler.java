56d9191198f6960542838d3047121eb00e870eff
Jailton Santos <jailtonmatos200@gmail.com>
2026-06-30 17:21:12 -0300

    feat: global exception implementation

---
+++ backend/src/main/java/levva/backend/exceptions/global/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package levva.backend.exceptions.global;
+
+import levva.backend.exceptions.api.ApiException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ApiException.class)
+    public ResponseEntity<Map<String, String>> handleApiException(ApiException ex) {
+        return ResponseEntity
+                .status(ex.getStatusCode())
+                .body(Map.of("error", ex.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleGenericException(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Map.of("error", "Erro interno no servidor"));
+    }
+}
